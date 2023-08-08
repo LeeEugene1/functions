2482ae0d442b2eea9869281c801bffcf3dcc97fa
eugene <uwm1004@gmail.com>
2023-08-08 14:29:59 +0900

    feat: klip mobile

--- nodejs/public/js/connectKlip.js
+++ nodejs/public/js/connectKlip.js
@@ -1,22 +1,62 @@
 let intervalId
 
-async function handleKilp(){
-    try {
-        const response = await fetch("https://a2a-api.klipwallet.com/v2/a2a/prepare", {
-            method:'post',
-            headers: { "Content-Type": "application/json" },
-            body:JSON.stringify({
-                bapp: {name:'bappName'},
-                type: 'auth'
+/**
+ * wallet-connect-utils
+ * Dependencies
+ * - web2app    : https://github.com/kakao/web2app
+ * - QRCode     : https://github.com/davidshimjs/qrcodejs
+ * - ua-parser  : https://github.com/faisalman/ua-parser-js
+ * - ethers     : https://docs.ethers.io
+ * - web3       : https://web3js.readthedocs.io/
+ */
+const connectMap = {
+    handleKilp: async () => {
+        try {
+            const response = await fetch("https://a2a-api.klipwallet.com/v2/a2a/prepare", {
+                method:'post',
+                headers: { "Content-Type": "application/json" },
+                body:JSON.stringify({
+                    bapp: {name:'bappName'},
+                    type: 'auth'
+                })
             })
-        })
+    
+            const {request_key} = await response.json()
 
-        const {request_key} = await response.json()
-        resetKlip()
-        makeQR(request_key)
-    } catch (error) {
-        console.error(error)
-    }
+            let device = get_device_state();
+            resetKlip()
+            if(device.isMobile){
+                daumtools.web2app({ // open deep link
+                    urlScheme: `kakaotalk://klipwallet/open?url=https://klipwallet.com/?target=/a2a?request_key=${request_key}`, // iphone : custom scheme
+                    intentURI: `intent://klipwallet/open?url=https://klipwallet.com/?target=/a2a?request_key=${request_key}#Intent;scheme=kakaotalk;package=com.kakao.talk;end`, // android : intent URI
+                    appName: 'Kakaotalk', // application Name (ex. facebook, twitter, daum)
+                    storeURL: device.isIos ? "itms-apps://itunes.apple.com/app/id362057947" : "market://details?id=com.kakao.talk", // app store URL
+                });
+            }else{
+                makeQR(request_key)
+            }
+            intervalId = setInterval(()=>{
+                poll(request_key)//완료시 clearInterval(intervalId)
+            },2000)
+        } catch (error) {
+            console.error(error)
+        }
+    },
+}
+
+function get_device_state() {
+    let parser = new UAParser();
+    let isAndroid = false;
+    let isIos = false;
+    let isDesktop = true;
+    if (parser.getOS().name === 'Android' || parser.getOS().name === 'Android-x86')
+        isAndroid = true;
+    else if (parser.getOS().name === 'iOS')
+        isIos = true;
+    if (isIos || isAndroid)
+        isDesktop = false;
+    let isMobile = !isDesktop;
+    return { "isIos": isIos, "isAndroid": isAndroid, "isDesktop": isDesktop, "isMobile": isMobile }
 }
 
 function makeQR(key){
@@ -26,9 +66,6 @@ function makeQR(key){
     }
     let qrContainer = document.getElementById('qr-container')
     new QRCode(qrContainer, `https://klipwallet.com/?target=/a2a?request_key=${key}`, qrOptions);
-    intervalId = setInterval(()=>{
-        poll(key)
-    },2000)
 }
 
 function resetKlip(){
@@ -57,7 +94,6 @@ async function poll(key){
  }
 
 const btnKlip = document.querySelector('#handleKilp')
+const btnKlipMobile = document.querySelector('#handleKilpMobile')
 
-btnKlip.addEventListener('click',()=>{
-    handleKilp()
-})
+btnKlip.addEventListener('click', connectMap.handleKilp)
--- readme.md
+++ readme.md
@@ -12,11 +12,11 @@ func
 10초 제한의 카운트 기능. 시작, 재시작 가능
 
 ### connectKlip.html
-클립지갑 로그인 기능(데스크톱)
-* 모바일 클립에 접근할수있는 QR코드를 생성
-* request_key를 생성하는 API를 호출
+클립지갑 로그인 기능(데스크톱+모바일)
+* 모바일 클립에 접근하여 지갑연동할 수 있는 기능
+* request_key를 생성하는 API를 호출(데스크톱에서 접근할경우 QR코드생성, 모바일에서 접근할경우 딥링크연동)
 * 모바일 클립과 연결상태를 체크하는 poll API를 2초마다 호출(setInterval)
-* 연결시 poll API를 해제후(clearInterval) 지갑주소 호출
+* 연결시 poll API를 해제후(clearInterval) 지갑주소 표시
 [참고] : https://docs.klipwallet.com/tutorial/tutorial-a2a-rest-api#case-1-auth
 
 ### changeLanguage
